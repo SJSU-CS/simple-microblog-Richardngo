1fa8df0a11cbf88fdbd14c7238b2d7056f1fb7c0
Richard Ngo <richard.ngo@roche.com>
2024-04-12 20:21:34 -0700

    Cleaning up code.

--- server/src/main/java/edu/sjsu/cmpe272/simpleblog/server/ServerApplication.java
+++ server/src/main/java/edu/sjsu/cmpe272/simpleblog/server/ServerApplication.java
@@ -4,13 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @SpringBootApplication
 // do not change these base packages otherwise /ruok stops working!
